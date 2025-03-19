28d6f7583a282f001bdce5c3ff62943d964759fa
nicksolari79 <nick.solari79@gmail.com>
2025-03-19 12:35:23 -0500

    userrole

---
+++ Selu383.SP25.P03.Api/Data/UserRoleNames.cs
@@ -0,0 +1,8 @@
+namespace Selu383.SP25.P03.Api.Data
+{
+    public static class UserRoleNames
+    {
+        public const string Admin = "Admin";
+        public const string User = "User"; // Add other roles as needed
+    }
+}
